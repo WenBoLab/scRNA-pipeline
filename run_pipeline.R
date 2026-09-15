58bced515bd7a8f74a790fd6ef3d2d1aa2169b08
flee70973-coder <flee70973@gmail.com>
2026-09-15 16:32:53 +0800

    Delete run_pipeline.R

--- run_pipeline.R
+++
@@ -1,4 +0,0 @@
-source("scripts/bootstrap.R")
-arguments <- if (sys.nframe() == 0L) commandArgs(trailingOnly = TRUE) else character()
-config_path <- if (length(arguments) && !startsWith(arguments[1], "--")) arguments[1] else "config/config.R"
-run_pipeline(config_path, resume = !"--no-resume" %in% arguments)
